af665fce1c2ef38b6511508cbe6498c1da5910eb
InnovatorZhang <1070042265@qq.com>
2017-01-17 21:59:53 +0800

    加入显示天气的功能。

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -34,7 +35,7 @@ import okhttp3.Response;
  * Created by 张 on 2017/1/16.
  */
 
-public class ChooseFragment extends Fragment {
+public class ChooseAreaFragment extends Fragment {
 
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
@@ -88,6 +89,13 @@ public class ChooseFragment extends Fragment {
                 }else if(currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if(currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by 张 on 2017/1/17.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
